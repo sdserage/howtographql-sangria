c36d65bceefd3a57c6e16b3a0c4263f39739e482
sdserage <xttocsurge51@gmail.com>
2018-09-19 13:02:24 -0600

    Added getLink and getLinks definitions

--- src/main/scala/com/howtographql/scala/sangria/DAO.scala
+++ src/main/scala/com/howtographql/scala/sangria/DAO.scala
@@ -2,7 +2,17 @@ package com.howtographql.scala.sangria
 
 import DBSchema._
 import slick.jdbc.H2Profile.api._
+import com.howtographql.scala.sangria.models.Link
+import scala.concurrent.Future
 
 class DAO(db: Database) {
   def allLinks = db.run(Links.result)
+
+  def getLink(id: Int): Future[Option[Link]] = db.run(
+    Links.filter(_.id === id).result.headOption
+  )
+
+  def getLinks(ids: Seq[Int]) = db.run(
+    Links.filter(_.id inSet ids).result
+  )
 }
