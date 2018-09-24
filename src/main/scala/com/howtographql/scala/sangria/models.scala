6fce555de6a037143ee4f6f0dfa69a4aee127d78
sdserage <xttocsurge51@gmail.com>
2018-09-24 09:23:19 -0600

    Added User and Vote to the models

--- src/main/scala/com/howtographql/scala/sangria/models.scala
+++ src/main/scala/com/howtographql/scala/sangria/models.scala
@@ -5,7 +5,12 @@ import sangria.validation.Violation
 
 package object models {
   case class Link(id: Int, url: String, description: String, createdAt: DateTime)
+
   case object DateTimeCoerceViolation extends Violation {
     override def errorMessage: String = "Error during parsing DateTime"
   }
+
+  case class User(id: Int, name: String, email: String, password: String, createdAt: DateTime)
+
+  case class Vote(id: Int, createdAt: DateTime, userId: Int, linkId: Int)
 }
