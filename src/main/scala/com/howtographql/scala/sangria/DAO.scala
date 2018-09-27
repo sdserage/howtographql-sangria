6de99bdecb34f98bf31ca80ccd178db03e4f4532
sdserage <xttocsurge51@gmail.com>
2018-09-26 16:38:27 -0800

    Added methods to retrieve Users and Votes

--- src/main/scala/com/howtographql/scala/sangria/DAO.scala
+++ src/main/scala/com/howtographql/scala/sangria/DAO.scala
@@ -2,11 +2,13 @@ package com.howtographql.scala.sangria
 
 import DBSchema._
 import slick.jdbc.H2Profile.api._
-import com.howtographql.scala.sangria.models.Link
+import com.howtographql.scala.sangria.models._
 import scala.concurrent.Future
 
 class DAO(db: Database) {
   def allLinks = db.run(Links.result)
+  def allUsers = db.run(Users.result)
+  def allVotes = db.run(Votes.result)
 
 //  def getLink(id: Int): Future[Option[Link]] = db.run(
 //    Links.filter(_.id === id).result.headOption
@@ -14,5 +16,13 @@ class DAO(db: Database) {
 
   def getLinks(ids: Seq[Int]) = db.run(
     Links.filter(_.id inSet ids).result
+  )
+
+  def getUsers(ids: Seq[Int]) = db.run(
+    Users.filter(_.id inSet ids).result
+  )
+
+  def getVotes(ids: Seq[Int]) = db.run(
+    Votes.filter(_.id inSet ids).result
   )
 }
